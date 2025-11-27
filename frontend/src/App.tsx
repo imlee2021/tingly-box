10097dfa27a8d7a7223dce7861236a656d20475d
FFengIll <fengyouzheng@gmail.com>
2025-11-27 16:51:23 +0800

    fmt.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
+import History from './pages/History';
 import Providers from './pages/Providers';
 import Server from './pages/Server';
-import History from './pages/History';
+import theme from './theme';
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/providers" element={<Providers />} />
-            <Route path="/server" element={<Server />} />
-            <Route path="/history" element={<History />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/providers" element={<Providers />} />
+                        <Route path="/server" element={<Server />} />
+                        <Route path="/history" element={<History />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
